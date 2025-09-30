2b01d67e6041e0321a31192d1ec5c73174e5d4c5
Alejandro Patiño García <alexpatino92@yahoo.com>
2025-09-30 20:33:16 +0200

    feat(database): added database session getter for dependency inyection in routes

--- Backend/src/backend/database.py
+++ Backend/src/backend/database.py
@@ -1,12 +1,25 @@
 from sqlmodel import SQLModel, create_engine
 from dotenv import load_dotenv
 from .models.users import User 
-
 import os
 
+###############################################
+# Database Configuration
+###############################################
 load_dotenv()
 database_url = os.getenv("DATABASE_URL")
 engine = create_engine(database_url, echo=True)
+
+###############################################
+# Database Helper Functions
+###############################################
+
+def get_session():
+    """Creates a new database session."""
+    from sqlmodel import Session
+    with Session(engine) as session:
+        yield session
+    
 
 
 def create_db_and_tables():
