ec54948732cb394d1696634feb5b22fec8cd5c45
Alejandro Patiño García <alexpatino92@yahoo.com>
2025-10-06 18:45:33 +0200

    chore(): reinstalled sveltekit and tailwindcss with daisyUI, now working

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex justify-center items-center mt-10 min-h-screen">
+  <button
+    class="btn btn-xl"
+    on:click={() => {
+      window.location.href = "/login";
+    }}>Sign in</button
+  >
+</div>
